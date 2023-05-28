db48e0496ea45b0142e1b9406c813f93d41a44b5
LizaShelkovskaya0705 <116936362+LizaShelkovskaya0705@users.noreply.github.com>
2023-05-28 19:40:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,68 +6,68 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-      BST<std::string> Mytree;
+    BST<std::string> Mytree;
     std::ifstream file(filename);
-    std::string v;
-    std::string v1 = "";
-    std::string v2 = "";
+    std::string w;
+    std::string w1 = "";
+    std::string w2 = "";
     while (!file.eof()) {
-        file >> v;
-        v1 = v2 = "";
-        if (v.find(",") != -1) {
-                int l = v.length();
-                int f = v.find(",");
-                if ((f != l - 1) && ((v[f + 1] >= 'A' && v[f + 1] <= 'Z') ||
-                    (v[f + 1] >= 'A' && v[f + 1] <= 'z'))) {
+        file >> w;
+        w1 = w2 = "";
+        if (w.find(",") != -1) {
+                int l = w.length();
+                int f = w.find(",");
+                if ((f != l - 1) && ((w[f + 1] >= 'A' && w[f + 1] <= 'Z') ||
+                    (w[f + 1] >= 'A' && w[f + 1] <= 'z'))) {
                 for (int i = 0; i < f; i++) {
-                    v1 += v[i];
+                    w1 += w[i];
                 }
                 for (int i = f+1; i < l; i++) {
-                    v2 += v[i];
+                    w2 += w[i];
                 }
             }
         }
-        if (v1 != "" && v2 != "") {
-            v1 = corr_word(v1);
-            v2 = corr_word(v2);
-            Mytree.add(v1);
-            Mytree.add(v2);
+        if (w1 != "" && w2 != "") {
+            w1 = get_corr_word(w1);
+            w2 = get_corr_word(w2);
+            Mytree.add(w1);
+            Mytree.add(w2);
         } else {
-            v = corr_word(v);
-            Mytree.add(v);
+            w = get_corr_word(w);
+            Mytree.add(w);
         }
     }
     return Mytree;
 }
 
-std::string corr_word(std::string r) {
-    if (r[0] < '0' || r[0] > '9') {
-        while ((r[0] >= 32 && r[0] <= 64) || (r[0] >= 91 && r[0] <= 96))
-        r.erase(0, 1);
-        if (r != "") {
-            char ch = r[r.length() - 1];
-            while ((ch >= 32 && ch <= 64) || (ch >= 91 && ch <= 96)) {
-                r.pop_back();
-                ch = r[r.length() - 1];
+std::string get_corr_word(std::string s) {
+    if (s[0] < '0' || s[0] > '9') {
+        while ((s[0] >= 32 && s[0] <= 64) || (s[0] >= 91 && s[0] <= 96))
+        s.erase(0, 1);
+        if (s != "") {
+            char ch = s[s.length() - 1];
+            while ((ch >= 32 && c <= 64) || (ch >= 91 && c <= 96)) {
+                s.pop_back();
+                ch = s[s.length() - 1];
             }
         }
         int it = 0;
-        if (r != "") {
-            for (int i = 0; i < r.length(); i++) {
-                if (r[i] == '\'') {
+        if (s != "") {
+            for (int i = 0; i < s.length(); i++) {
+                if (s[i] == '\'') {
                     it = i;
                 }
             }
             if (it != 0)
-                r.erase(it);
+                s.erase(it);
         }
-        if (r != "") {
-            for (int i = 0; i < r.length(); i++) {
-                r[i] = tolower(r[i]);
+        if (s != "") {
+            for (int i = 0; i < s.length(); i++) {
+                s[i] = tolower(s[i]);
             }
         }
     } else {
-        r.clear();
+        s.clear();
     }
-    return r;
+    return s;
 }
